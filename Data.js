a0ab92851cac1b89d14fed293a741ee3d774d338
Khulood-Ebrahim <khuloodebrahim@live.com>
2017-12-26 11:37:54 +0800

    Add data.js

---
+++ Data.js
@@ -0,0 +1,34 @@
+window.onload = function() {
+
+var dataPoints = [];
+
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	theme: "light2",
+	title:{
+		text: "Sales Report"
+	},
+	axisY:{
+        title: "Sales",
+		includeZero: false,
+        valueFormatString:"$#######"
+	},
+    axisX:{
+        title: "Date",
+		includeZero: false
+	},
+	data: [{
+		type: "line",
+		dataPoints: dataPoints
+	}]
+});
+
+function addData(data) {
+	
+	}
+	chart.render();
+
+}
+
+$.getJSON("http://localhost:8000/mobiledash/Test.json", function(addData)
+
